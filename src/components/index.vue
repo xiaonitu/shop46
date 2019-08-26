e6173a54c8b732843ca0f84bbdc9dd8f07a74925
xiaonitu <1123017460@qq.com>
2019-08-26 16:03:31 +0800

    这是开始

---
+++ src/components/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="index">这是主页</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="login">这是登录</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
